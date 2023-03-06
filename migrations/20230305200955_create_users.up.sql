1b3220f976894bdfcc2dcdd98bbe2a41b62da1ad
SicParv1sMagna <92353726+SicParv1sMagna@users.noreply.github.com>
2023-03-06 07:12:23 +0300

    миграции уже не нужны

--- migrations/20230305200955_create_users.up.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE users (
-    id bigserial not null primary key,
-    email varchar not null unique,
-    encrypted_password varchar not null,
-    first_name varchar not null,
-    second_name varchar not null,
-    card_number varchar,
-    card_cvv int
-)
