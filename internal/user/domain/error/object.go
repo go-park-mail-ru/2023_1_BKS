4bf64e221fc2482243e834bf4246296b47f8b841
Долгих Артём <artem144565@gmail.com>
2023-03-30 23:36:50 +0300

    Дополнение

--- internal/user/domain/error/object.go
+++ internal/user/domain/error/object.go
@@ -57,14 +57,39 @@ func (e *LoginAcceptableValuesErr) Error() string {
 	return fmt.Sprintf("Логин содержит недопустимые символы: %s\n", str)
 }
 
-type FirstNameErr struct{}
+type FirstNameLengthErr struct{}
 
 func (e *FirstNameErr) Error() string {
 	return "Имя не было введено"
 }
 
-type SecondNameErr struct{}
+type SecondNameLengthErr struct{}
 
-func (e *SecondNameErr) Error() string {
+func (e *SecondNameLengthErr) Error() string {
 	return "Фамилия не была введена"
+}
+
+type CompanyTypeErr struct {
+	typeComp []string
+}
+
+func (e *CompanyTypeErr) Error() string {
+	var str string
+	for _, typeComp := range e.typeComp {
+		str = fmt.Sprintf(str, typeComp, ", ")
+	}
+	str = str[0 : len(str)-3]
+	return fmt.Sprintf("Неправильные тип компании. Введите допустимый из данного списка: %s\n", str)
+}
+
+type CompanyNameLengthErr struct{}
+
+func (e *CompanyNameLengthErr) Error() string {
+	return "Название компании не введено"
+}
+
+type AdressRequiredValueErr struct{}
+
+func (e *AdressRequiredValueErr) Error() string {
+	return "Введён не валидный адресс"
 }
