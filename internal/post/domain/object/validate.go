bbbeeafb7a0dc858220f36cbd54d8500486a5d79
TimDrake <92353726+SicParv1sMagna@users.noreply.github.com>
2023-04-02 15:47:03 +0300

    Update validate.go

--- internal/post/domain/object/validate.go
+++ internal/post/domain/object/validate.go
@@ -1,15 +1,43 @@
 package object
 
-import "image"
+import (
+	"image"
 
-func ValidateTitle(body string) (bool, error) {
-	return true, nil
+	err "github.com/go-park-mail-ru/2023_1_BKS/internal/post/domain/error"
+)
+
+func ValidateTitle(title string, maxTitleLength int) error {
+	if len(title) == 0 {
+		return &err.TitleEmptyErr{}
+	}
+
+	if len(title) > maxTitleLength {
+		return &err.LongTitleErr{MaxLength: maxTitleLength}
+	}
+
+	return nil
 }
 
-func ValidateBody(body string) (bool, error) {
-	return true, nil
+func ValidateBody(body string, maxBodyLength int) error {
+	if len(body) == 0 {
+		return &err.BodyEmptyErr{}
+	}
+
+	if len(body) > maxBodyLength {
+		return &err.LongBodyErr{MaxLength: maxBodyLength}
+	}
+
+	return nil
 }
 
-func ValidateImage(image []image.RGBA) (bool, error) {
-	return true, nil
+func ValidateImages(images []*image.RGBA, maxImageCount int) error {
+	if len(images) == 0 {
+		return &err.ImageEmptyErr{}
+	}
+
+	if len(images) > maxImageCount {
+		return &err.ManyImagesErr{MaxCount: maxImageCount}
+	}
+
+	return nil
 }
